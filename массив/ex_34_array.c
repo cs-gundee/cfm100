7b225b26a632575e72cce7a5d97998e9a2191674
csgundee <66056983+cs-gundee@users.noreply.github.com>
2025-09-26 18:53:01 +0800

    Create ex_34_array.c

---
+++ массив/ex_34_array.c
@@ -0,0 +1,14 @@
+/* Ex_34 The name of an array is the same as &array[ 0 ] */
+#include <stdio.h>
+ 
+/* function main begins program execution */
+int main( void )
+{
+   char array[ 5 ]; /* define an array of size 5 */
+ 
+   printf( "    array = %p\n&array[0] = %p\n   &array = %p\n",
+      array, &array[ 0 ], &array );
+ 
+   return 0; /* indicates successful termination */
+ 
+} /* end main */
